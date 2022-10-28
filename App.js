27a1536656b39f4817f84b1cb680411e35259bf7
Lucas Bolela <lucasbolelasilveira@gmail.com>
2022-10-28 15:59:42 -0300

    fix: App.js base

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthStack } from "./src/navigation/AuthStack";
+
+
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <AuthStack />
+        </NavigationContainer>
+    );
+}
