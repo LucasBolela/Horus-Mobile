a96b5a36954bee0497d6c17d09ad30a9157f67ca
Lucas Bolela <lucasbolelasilveira@gmail.com>
2022-10-29 21:49:58 -0300

    add: Input Field

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -4,40 +4,52 @@ import { View, Text, TouchableOpacity, TextInput } from "react-native";
 export default function InputField({
     label, 
     icon,
+    onChange,
     inputType,
+    errorMessage,
     keyboardType,
     fieldButtonLabel,
-    fieldButtonFunction
+    fieldButtonFunction,
 }) {
     return (
-        <View
-            style={{
-                flexDirection: 'row',
-                borderBottomColor: '#ccc',
-                borderBottomWidth: 1,
-                paddingBottom: 8,
-                marginBottom: 25,
-                alignItems: 'center'
-            }}
-        >
-            {icon}
-            {inputType == 'password' ? (
-                <TextInput 
-                    placeholder={label}
-                    keyboardType={keyboardType}
-                    style={{flex: 1, paddingVertical: 0}}
-                    secureTextEntry={true}
-                />
-            ) : (
-                <TextInput 
-                    placeholder={label}
-                    keyboardType={keyboardType}
-                    style={{flex: 1, paddingVertical: 0}}
-                />
-            )}
-            <TouchableOpacity onPress={fieldButtonFunction}>
-                <Text style={{color: '#227bed', fontWeight: 'bold'}}>{fieldButtonLabel}</Text>
-            </TouchableOpacity>
-        </View>
+        <>
+
+            <View
+                style={{
+                    flexDirection: 'row',
+                    borderBottomColor: errorMessage ? 'red' : '#ccc',
+                    borderBottomWidth: 1,
+                    paddingBottom: 8,
+                    marginBottom: 25,
+                    alignItems: 'center'
+                }}
+            >
+                {icon}
+                {inputType == 'password' ? (
+                    <TextInput 
+                        placeholder={label}
+                        keyboardType={keyboardType}
+                        style={{flex: 1, paddingVertical: 0}}
+                        secureTextEntry={true}
+                        onChangeText={onChange}
+                        errorMessage={errorMessage}
+                    />
+                    ) : (
+                        <TextInput 
+                        placeholder={label}
+                        keyboardType={keyboardType}
+                        style={{flex: 1, paddingVertical: 0}}
+                        onChangeText={onChange}
+                        errorMessage={errorMessage}
+                    />
+                )}
+                <TouchableOpacity onPress={fieldButtonFunction}>
+                    <Text style={{color: '#227bed', fontWeight: 'bold'}}>{fieldButtonLabel}</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={{color: 'red', top: -20}}>
+                {errorMessage}
+            </Text>
+        </>
     );
 }
